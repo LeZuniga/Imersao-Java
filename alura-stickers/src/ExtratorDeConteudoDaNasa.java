d7822d7e56967421bdfc913c7ef9dd655743aa8c
LeZuniga <leh.zuniga@gmail.com>
2022-08-01 22:18:38 -0300

    trabalhando consumo de dados da API da NASA

---
+++ alura-stickers/src/ExtratorDeConteudoDaNasa.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class ExtratorDeConteudoDaNasa  {
+
+    public List<Conteudo> extraiConteudos(String json) {
+        // extrair só os dados que interessam (titulo, poster, classificação)
+        var parser = new JsonParser();
+        List<Map<String, String>> listaDeAtributos = parser.parse(json);
+
+        List<Conteudo> conteudos = new ArrayList<>();
+
+        // popular a lista de conteudos
+        for (Map<String, String> atributos : listaDeAtributos) {
+            conteudos.add(new Conteudo(atributos.get("title"), atributos.get("url")));
+        }
+
+        return conteudos;
+    }
+}
